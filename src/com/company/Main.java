184fa240d0d91e6917d885b7cc6d77da0bc34219
Tesfabrhan Naizghi <tesfienaiz gmail.com>
2020-06-18 15:44:03 +0300

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,11 +6,11 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner me = new Scanner(System.in);
-        System.out.println("Enter your number to win a million dollar: ");
+        System.out.println("Enter a number between 0 and 100 inclusive to win ten thousands dollar: ");
         int myNumber = me.nextInt();
-        int randomNumber = (int) (Math.random() * 10);
+        int randomNumber = (int) (Math.random() * 100);
         if (myNumber == randomNumber) {
-            System.out.println("You have won 1000000");
+            System.out.println("You have won 10000");
         }
         else
             System.out.println("Sorry, U didn't win any thing now. Try again later !");
